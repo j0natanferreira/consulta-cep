ff5750e36151415b2beb846692960a0570148a88
Jonatan Ferreira <82197119+j0natanferreira@users.noreply.github.com>
2023-10-18 00:01:44 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,16 @@ async function consultarCep(){
         // Input que recebe o CEP
         let cep = document.querySelector(".cep").value
 
-        if (cep == "") {
-            alert("Campo Vazio!");
-            return;
-        };
-
         // Api Consulta CEP
         const response = await fetch('https://viacep.com.br/ws/'+ cep + '/json');
         const ceps = await response.json();
 
         // Inserindo dados na arvore do DOM
-        let cep1 = document.querySelector(".cep1").innerHTML = `CEP: ` + (ceps.cep ?? 'Não encontrado');
-        let logradouro = document.querySelector(".logradouro").innerHTML = `Endereço: ` + (ceps.logradouro ?? '');
-        let bairro = document.querySelector(".bairro").innerHTML = `Bairro: ` + (ceps.bairro ?? '');
-        let localidade = document.querySelector(".localidade").innerHTML = `CEP: ` + (ceps.bairro ?? '');
-        let uf = document.querySelector(".uf").innerHTML = `UF: ` + (ceps.uf ?? ''); 
+        let cep1 = document.querySelector(".cep1").innerHTML = `CEP: ` + ceps.cep;
+        let logradouro = document.querySelector(".logradouro").innerHTML = `Endereço: ` + ceps.logradouro;
+        let bairro = document.querySelector(".bairro").innerHTML = `Bairro: ` + ceps.bairro;
+        let localidade = document.querySelector(".localidade").innerHTML = `Cidade: ` + ceps.localidade;
+        let uf = document.querySelector(".uf").innerHTML = `UF: ` + ceps.uf; 
 
         //retorna o input vazio
         document.querySelector(".cep").value =""
