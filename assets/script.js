b364e3fa49500c3904487c7d9e6029c6fe40e4eb
Jonatan Ferreira <jonatan91668954@gmail.com>
2023-10-17 23:41:34 -0300

    Atualização

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
 async function consultarCep(){
-    // Input que recebe o CEP
-    let cep = document.querySelector(".cep").value
+    try {
+        // Input que recebe o CEP
+        let cep = document.querySelector(".cep").value
 
-    if (cep == "") {
-        alert("Campo Vazio!")    
-    };
+        if (cep == "") {
+            alert("Campo Vazio!");
+            return;
+        };
 
-    // Api Consulta CEP
-    const response = await fetch('https://viacep.com.br/ws/'+ cep + '/json');
-    const ceps = await response.json();
+        // Api Consulta CEP
+        const response = await fetch('https://viacep.com.br/ws/'+ cep + '/json');
+        const ceps = await response.json();
 
-    // Inserindo dados na arvore do DOM
-    let cep1 = document.querySelector(".cep1").innerHTML = `CEP: ${ceps.cep}`;
-    let logradouro = document.querySelector(".logradouro").innerHTML = `Endereço: ${ceps.logradouro}`
-    let bairro = document.querySelector(".bairro").innerHTML = `Bairro: ${ceps.bairro}`;
-    let localidade = document.querySelector(".localidade").innerHTML = `CEP: ${ceps.localidade}`;
-    let uf = document.querySelector(".uf").innerHTML = `UF: ${ceps.uf}`; 
+        // Inserindo dados na arvore do DOM
+        let cep1 = document.querySelector(".cep1").innerHTML = `CEP: ` + (ceps.cep ?? 'Não encontrado');
+        let logradouro = document.querySelector(".logradouro").innerHTML = `Endereço: ` + (ceps.logradouro ?? '');
+        let bairro = document.querySelector(".bairro").innerHTML = `Bairro: ` + (ceps.bairro ?? '');
+        let localidade = document.querySelector(".localidade").innerHTML = `CEP: ` + (ceps.bairro ?? '');
+        let uf = document.querySelector(".uf").innerHTML = `UF: ` + (ceps.uf ?? ''); 
 
+        //retorna o input vazio
+        document.querySelector(".cep").value =""
+    }catch{
+        alert("CEP não encontrado!");
+            return;
+    }
 }
